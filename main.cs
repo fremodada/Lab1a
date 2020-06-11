a807b1c7f753a623b7dd50bf649f7fe2e546a823
Freddie Mowery <fredmowery@gmail.com>
2020-06-11 01:00:16 +0000

    Added some text indicating that only whole numbers allowed for quantity.

--- main.cs
+++ main.cs
@@ -2,6 +2,18 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+
+    // Get input
+    Console.WriteLine ("Enter Item Name");
+    string itemName = Console.ReadLine();
+    Console.WriteLine("Enter Item Quantity (only whole numbers)");
+    int quantity = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter Item Price");
+    double price = Convert.ToDouble(Console.ReadLine());
+    // Calculate Total Price
+   double totalPrice = quantity * price;
+   // Display output
+   Console.WriteLine("Total price of " + itemName + " is $" + totalPrice );
   }
 }
